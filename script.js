98abb53b28d5f44af3457f5a12fba66b921c59be
isabell-lee08 <isabellleee8@gmail.com>
2025-09-14 14:52:30 -0400

    filled out template

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // 1. Start with an array of strings (ex: "grapes", "bread", "tea")
 let todoItems = [
     // add more items here
+    "grapes", "bread", "tea"
 ];
 
 
@@ -11,43 +12,51 @@ let todoItems = [
 const addItemButton = document.getElementById('add-item-button');
 // add more variables below
 const list = document.getElementById('list');
+const sortBtn = document.getElementById('sort');
+const clearBtn = document.getElementById('clear');
+const input = document.getElementById('text');
 
 
 
 // 3. Write a function to display all items in the #list element
-function updateList() {
+function updateList(index, items) {
     // add your code here
+
+    let li = document.createElement('li');
+    li.innerText = index, items;
+    list.appendChild(li);
+
 }
 
-updateList();
-
-
+todoItems.forEach(updateList)
 
 // 4. Handle adding a new item when the form is submitted
 addItemButton.addEventListener('click', function () {
     // add your code here
+    todoItems.push(input.value);
+    updateList(todoItems[todoItems.length - 1]);
 });
-
-
-
 
 // 5. Sort items alphabetically when sortBtn is clicked
 sortBtn.addEventListener("click", () => {
     // add your code here
+    list.innerHTML = '';
+    todoItems.sort();
+    todoItems.forEach(updateList);
 });
 
 
 
-
-// 6. Transform items to uppercase when upperBtn is clicked
-upperBtn.addEventListener("click", () => {
-    // add your code here
-});
-
+// // 6. Transform items to uppercase when upperBtn is clicked
+// upperBtn.addEventListener("click", () => {
+//     // add your code here
+// });
 
 
 
 // 7. Clear all items when clearBtn is clicked
 clearBtn.addEventListener("click", () => {
     // add your code here
+    todoItems.length = 0;
+    list.innerHTML = '';
 });
