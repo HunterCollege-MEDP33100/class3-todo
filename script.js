250494f9f3ae34d044c213cfb683f7eb0627345a
Jorge Auquilla <jorge.auquilla99@myhunter.cuny.edu>
2025-09-17 16:23:40 -0400

    javascript hw3

--- script.js
+++ script.js
@@ -10,7 +10,13 @@ let todoItems = ["bananas", "rice","bread"
 // 2. Create variables for each interactive DOM element
 const addItemButton = document.getElementById('add-item-button');
 // add more variables below
+const text = document.getElementById('text');
 const list = document.getElementById('list');
+
+const sortBtn = document.getElementById('sort');
+const clearBtn = document.getElementById('clear');
+
+
 
 
 
@@ -19,17 +25,17 @@ function updateList() {
     // add your code here
 }
 
-updateList();
+// updateList();
 
 
 
 // 4. Handle adding a new item when the form is submitted
-addItemButton.addEventListener('click', function () {
-    // add your code here
+addItemButton.addEventListener('click', function (item) {
+    return todoItems.push(item);
 });
 
 
-
+console.log(todoItems);
 
 // 5. Sort items alphabetically when sortBtn is clicked
 sortBtn.addEventListener("click", () => {
@@ -39,15 +45,14 @@ sortBtn.addEventListener("click", () => {
 
 
 
-// 6. Transform items to uppercase when upperBtn is clicked
-upperBtn.addEventListener("click", () => {
-    // add your code here
-});
+// // 6. Transform items to uppercase when upperBtn is clicked
+// upperBtn.addEventListener("click", () => {
+//     // add your code here
+// });
 
 
 
 
 // 7. Clear all items when clearBtn is clicked
 clearBtn.addEventListener("click", () => {
-    // add your code here
 });
