58d7cf4b4698ae35ffd4d950e8ce0c3f2e7a5f0a
DanielM0217 <daniel.martinez27@login.cuny.edu>
2025-09-17 20:13:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,28 @@
 
 // 1. Start with an array of strings (ex: "grapes", "bread", "tea")
 let todoItems = [
+    "grapes", "bread", "tea" 
     // add more items here
 ];
-
-
 
 // 2. Create variables for each interactive DOM element
 const addItemButton = document.getElementById('add-item-button');
 // add more variables below
 const list = document.getElementById('list');
 
+const sortBtn = document.getElementById('sort');
 
+const clearBtn = document.getElementById('clear');
+
+let inputvalue = document.getElementById('text').value;
 
 // 3. Write a function to display all items in the #list element
 function updateList() {
-    // add your code here
+    document.getElementById("list").innerHTML = ''
+    for (let i = 0; i < todoItems.length-1; i++){
+        document.getElementById("list").innerHTML += todoItems[i] +', '
+    }
+        document.getElementById("list").innerHTML += todoItems[todoItems.length-1]
 }
 
 updateList();
@@ -25,15 +32,17 @@ updateList();
 
 // 4. Handle adding a new item when the form is submitted
 addItemButton.addEventListener('click', function () {
-    // add your code here
-});
+    let inputvalue = document.getElementById('text').value;
+    todoItems.push(inputvalue);
+    updateList()}
+);
 
-
-
+console.log(todoItems)
 
 // 5. Sort items alphabetically when sortBtn is clicked
 sortBtn.addEventListener("click", () => {
-    // add your code here
+    todoItems.sort();
+    updateList()
 });
 
 
@@ -41,5 +50,6 @@ sortBtn.addEventListener("click", () => {
 
 // 6. Clear all items when clearBtn is clicked
 clearBtn.addEventListener("click", () => {
-    // add your code here
+    todoItems = [''];
+    updateList();
 });
